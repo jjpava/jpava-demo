a3f8518982eca17b859673f2e82de54e458fb48a
Dmitry Barskov <barskoidim@gmail.com>
2020-02-01 10:16:57 +0300

    Create tests for Specification usage

---
+++ src/test/java/io/github/jjpava/jpavademo/TextSpecificationTest.java
@@ -0,0 +1,61 @@
+package io.github.jjpava.jpavademo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static io.github.jjpava.jpava.specifications.TextSpecifications.withText;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TextSpecificationTest {
+    private final PostsRepository postsRepository;
+
+    @Autowired
+    public TextSpecificationTest(PostsRepository postsRepository) {
+        this.postsRepository = postsRepository;
+    }
+
+    Post wordsEverywhere = new Post("has 'word' in title", "has 'word' in text");
+    Post wordInTitle = new Post("has 'word' in title", "has no 'w0rd' in text");
+    Post wordInText = new Post("has no 'w0rd' in title", "has no 'word' in text");
+    Post noWords = new Post("has no 'w0rd' in title", "has no 'w0rd' in text");
+
+    List<Post> searchResult;
+
+    @BeforeEach
+    public void fillDatabase() {
+        postsRepository.save(wordsEverywhere);
+        postsRepository.save(wordInTitle);
+        postsRepository.save(wordInText);
+        postsRepository.save(noWords);
+        searchResult = postsRepository.findAll(withText("Word").inAnyColumnOf(Post.class));
+    }
+
+    @Test
+    public void shouldReturnPostWithSubstringInPreview() {
+        assertTrue(searchResult.contains(wordInTitle));
+    }
+
+    @Test
+    public void shouldReturnPostWithSubstringInEveryColumn() {
+        assertTrue(searchResult.contains(wordsEverywhere));
+    }
+
+    @Test
+    public void shouldReturnPostWithSubstringInText() {
+        assertTrue(searchResult.contains(wordInText));
+    }
+
+    @Test
+    public void shouldNotReturnPostWithNoSubstringAtAll() {
+        assertFalse(searchResult.contains(noWords));
+    }
+}
